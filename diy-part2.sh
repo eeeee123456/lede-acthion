1e94731bcd0fe5f99bc148ba7da44734efc780c1
eeeee123456 <79471015+eeeee123456@users.noreply.github.com>
2022-09-06 18:00:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,12 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+#建立目录
+mkdir package/diy
+#过校园网检测
+git clone https://github.com/EOYOHOO/UA2F.git package/UA2F package/diy/UA2F
+git clone https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid package/diy/rkp-ipid
+
+#主题 
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/diy/argon
