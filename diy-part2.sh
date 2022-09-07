1e0cb167e7d6f83480d6dd6d5c793fb4dfdb5ece
eeeee123456 <79471015+eeeee123456@users.noreply.github.com>
2022-09-07 22:36:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,13 +18,3 @@ mkdir package/diy
 #过校园网检测
 git clone https://github.com/EOYOHOO/UA2F.git package/UA2F package/diy/UA2F
 git clone https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid package/diy/rkp-ipid
-
-#主题 
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/diy/argon
-
-git clone https://github.com/monokoo/luci-app-arpbind.git package/diy/arpbind
-git clone https://github.com/zxl78585/luci-app-autoreboot.git package/diy/autoreboot
-git clone https://github.com/zxl78585/luci-app-filetransfer.git package/diy/filetransfer
-
-#git clone https://github.com/mchome/openwrt-vlmcsd.git package/diy/vlmcsd
-#git clone https://github.com/mchome/luci-app-vlmcsd.git package/diy/luci-vlmcsd
